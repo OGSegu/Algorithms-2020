925cee07d5a4f53238f15b314efe0201dfa580e5
OGSegu <seguisreal@gmail.com>
2020-09-28 18:59:23 +0300

    sortTemperatures has been done

--- src/lesson1/JavaTasks.java
+++ src/lesson1/JavaTasks.java
@@ -2,36 +2,44 @@ package lesson1;
 
 import kotlin.NotImplementedError;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.*;
+
+import static lesson1.Sorts.mergeSort;
+
 @SuppressWarnings("unused")
 public class JavaTasks {
     /**
      * Сортировка времён
-     *
+     * <p>
      * Простая
      * (Модифицированная задача с сайта acmp.ru)
-     *
+     * <p>
      * Во входном файле с именем inputName содержатся моменты времени в формате ЧЧ:ММ:СС AM/PM,
      * каждый на отдельной строке. См. статью википедии "12-часовой формат времени".
-     *
+     * <p>
      * Пример:
-     *
+     * <p>
      * 01:15:19 PM
      * 07:26:57 AM
      * 10:00:03 AM
      * 07:56:14 PM
      * 01:15:19 PM
      * 12:40:31 AM
-     *
+     * <p>
      * Отсортировать моменты времени по возрастанию и вывести их в выходной файл с именем outputName,
      * сохраняя формат ЧЧ:ММ:СС AM/PM. Одинаковые моменты времени выводить друг за другом. Пример:
-     *
+     * <p>
      * 12:40:31 AM
      * 07:26:57 AM
      * 10:00:03 AM
      * 01:15:19 PM
      * 01:15:19 PM
      * 07:56:14 PM
-     *
+     * <p>
      * В случае обнаружения неверного формата файла бросить любое исключение.
      */
     static public void sortTimes(String inputName, String outputName) {
@@ -40,44 +48,81 @@ public class JavaTasks {
 
     /**
      * Сортировка адресов
-     *
+     * <p>
      * Средняя
-     *
+     * <p>
      * Во входном файле с именем inputName содержатся фамилии и имена жителей города с указанием улицы и номера дома,
      * где они прописаны. Пример:
-     *
+     * <p>
      * Петров Иван - Железнодорожная 3
      * Сидоров Петр - Садовая 5
      * Иванов Алексей - Железнодорожная 7
      * Сидорова Мария - Садовая 5
      * Иванов Михаил - Железнодорожная 7
-     *
+     * <p>
      * Людей в городе может быть до миллиона.
-     *
+     * <p>
      * Вывести записи в выходной файл outputName,
      * упорядоченными по названию улицы (по алфавиту) и номеру дома (по возрастанию).
      * Людей, живущих в одном доме, выводить через запятую по алфавиту (вначале по фамилии, потом по имени). Пример:
-     *
+     * <p>
      * Железнодорожная 3 - Петров Иван
      * Железнодорожная 7 - Иванов Алексей, Иванов Михаил
      * Садовая 5 - Сидоров Петр, Сидорова Мария
-     *
+     * <p>
      * В случае обнаружения неверного формата файла бросить любое исключение.
      */
-    static public void sortAddresses(String inputName, String outputName) {
-        throw new NotImplementedError();
+    // Эффективность - O(nlogn)
+    // Ресурсы - O(n)
+    static public void sortAddresses(String inputName, String outputName) throws IOException {
+        FileWriter fileWriter = new FileWriter(new File(outputName), StandardCharsets.UTF_8);
+        Map<String, TreeSet<String>> treeMap = new TreeMap<>((s1, s2) -> {
+            String[] info1 = s1.split(" ");
+            String[] info2 = s2.split(" ");
+            if (!info1[0].equals(info2[0])) {
+                return info1[0].compareTo(info2[0]);
+            } else {
+                int firstNumber = Integer.parseInt(info1[1]);
+                int secondNumber = Integer.parseInt(info2[1]);
+                return Integer.compare(firstNumber, secondNumber);
+            }
+        });
+        BufferedReader br = new BufferedReader(new FileReader(inputName, StandardCharsets.UTF_8));
+        String line = br.readLine();
+        while (line != null) {
+            String[] infoArray = line.split(" - ");
+            if (!treeMap.containsKey(infoArray[1])) {
+                treeMap.put(infoArray[1], new TreeSet<>());
+            }
+            treeMap.get(infoArray[1]).add(infoArray[0]);
+            line = br.readLine();
+        }
+        String[] keys = treeMap.keySet().toArray(new String[0]);
+        for (String key : keys) {
+            StringBuilder sb = new StringBuilder(key).append(" - ");
+            for (String name : treeMap.get(key)) {
+                sb.append(name);
+                if (!treeMap.get(key).last().equals(name)) {
+                    sb.append(", ");
+                } else {
+                    sb.append("\n");
+                }
+            }
+            fileWriter.write(sb.toString());
+        }
+        fileWriter.close();
     }
 
     /**
      * Сортировка температур
-     *
+     * <p>
      * Средняя
      * (Модифицированная задача с сайта acmp.ru)
-     *
+     * <p>
      * Во входном файле заданы температуры различных участков абстрактной планеты с точностью до десятых градуса.
      * Температуры могут изменяться в диапазоне от -273.0 до +500.0.
      * Например:
-     *
+     * <p>
      * 24.7
      * -12.6
      * 121.3
@@ -85,11 +130,11 @@ public class JavaTasks {
      * 99.5
      * -12.6
      * 11.0
-     *
+     * <p>
      * Количество строк в файле может достигать ста миллионов.
      * Вывести строки в выходной файл, отсортировав их по возрастанию температуры.
      * Повторяющиеся строки сохранить. Например:
-     *
+     * <p>
      * -98.4
      * -12.6
      * -12.6
@@ -98,18 +143,41 @@ public class JavaTasks {
      * 99.5
      * 121.3
      */
-    static public void sortTemperatures(String inputName, String outputName) {
-        throw new NotImplementedError();
+    // Эффективность - O(nlogn)
+    // Ресурсы - O(n)
+    static public void sortTemperatures(String inputName, String outputName) throws IOException, ParseException {
+        FileWriter fileWriter = new FileWriter(new File(outputName), StandardCharsets.UTF_8);
+        BufferedReader br = new BufferedReader(new FileReader(inputName, StandardCharsets.UTF_8));
+        TreeMap<Double, Integer> treeMap = new TreeMap<>();
+        String line = br.readLine();
+        while (line != null) {
+            double number = Double.parseDouble(line);
+            if (treeMap.containsKey(number)) {
+                treeMap.put(number, treeMap.get(number) + 1);
+            } else {
+                treeMap.put(number, 1);
+            }
+            line = br.readLine();
+        }
+        Double[] keys = treeMap.keySet().toArray(new Double[0]);
+        for (double key : keys) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < treeMap.get(key); i++) {
+                sb.append(key).append("\n");
+            }
+            fileWriter.write(sb.toString());
+        }
+        fileWriter.close();
     }
 
     /**
      * Сортировка последовательности
-     *
+     * <p>
      * Средняя
      * (Задача взята с сайта acmp.ru)
-     *
+     * <p>
      * В файле задана последовательность из n целых положительных чисел, каждое в своей строке, например:
-     *
+     * <p>
      * 1
      * 2
      * 3
@@ -117,12 +185,12 @@ public class JavaTasks {
      * 3
      * 1
      * 2
-     *
+     * <p>
      * Необходимо найти число, которое встречается в этой последовательности наибольшее количество раз,
      * а если таких чисел несколько, то найти минимальное из них,
      * и после этого переместить все такие числа в конец заданной последовательности.
      * Порядок расположения остальных чисел должен остаться без изменения.
-     *
+     * <p>
      * 1
      * 3
      * 3
@@ -137,16 +205,16 @@ public class JavaTasks {
 
     /**
      * Соединить два отсортированных массива в один
-     *
+     * <p>
      * Простая
-     *
+     * <p>
      * Задан отсортированный массив first и второй массив second,
      * первые first.size ячеек которого содержат null, а остальные ячейки также отсортированы.
      * Соединить оба массива в массиве second так, чтобы он оказался отсортирован. Пример:
-     *
+     * <p>
      * first = [4 9 15 20 28]
      * second = [null null null null null 1 3 9 13 18 23]
-     *
+     * <p>
      * Результат: second = [1 3 4 9 9 13 15 20 23 28]
      */
     static <T extends Comparable<T>> void mergeArrays(T[] first, T[] second) {
